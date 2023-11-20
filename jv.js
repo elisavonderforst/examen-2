f2f214295b3183cd89c1d7a9c69acc2f52030bf2
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2023-11-20 16:58:52 +0100

    essaie de javascripte

---
+++ jv.js
@@ -0,0 +1,12 @@
+var menu = document.querySelectore(".menu-two");
+var button = document.getElementById(".button-menu");
+
+var openMenu = function () {
+  button.classList.toggle("is-open");
+};
+
+menuopen.addEventListener("click", .button-menu);
+
+if ("click", openMenu) {
+  openMenu.style.display = "block";
+}
