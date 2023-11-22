f4f6c1181f752ed86772212a17e132ce6c8e8ce7
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2023-11-22 19:04:29 +0100

    javascript menu et burger

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var one = document.querySelector(".menu-two");
+var two = document.querySelector(".button-menu");
+var three = document.querySelector(".burger-menu-open");
+var four = document.querySelector(".button-burger");
+var five = document.querySelector(".burger-menu");
+var six = document.querySelector(".a-click");
+var seven = document.querySelector(".main");
+var eight = document.querySelector(".img-header");
+var nine = document.querySelector(".footer");
+var ten = document.querySelector(".evenements");
+
+var changeOne = function () {
+  one.classList.toggle("is-hidden");
+};
+
+two.addEventListener("click", changeOne);
+
+var burgerMobile = function () {
+  three.classList.toggle("is-open");
+  five.classList.toggle("is-close");
+  seven.classList.toggle("is-covert");
+  eight.classList.toggle("is-covert");
+  nine.classList.toggle("is-covert");
+};
+
+four.addEventListener("click", burgerMobile);
+
+var burgerMobileClose = function () {
+  three.classList.toggle("is-covert");
+  seven.classList.toggle("is-visible");
+  eight.classList.toggle("is-visible");
+  nine.classList.toggle("is-visible");
+  five.classList.toggle("is-visible");
+  ten.classList.toggle("is-visible");
+};
+
+six.addEventListener("click", burgerMobileClose);
