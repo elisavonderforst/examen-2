e661c8795a2e5a8390e3ba3bfd285cb2f647aa87
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2023-12-06 14:25:00 +0100

    JS enfin réussi :) ainsi que le tri des balises

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var two = document.querySelector(".button-menu");
 var three = document.querySelector(".burger-menu-open");
 var four = document.querySelector(".button-burger");
 var five = document.querySelector(".burger-menu");
-var six = document.querySelector(".a-click");
+var six = document.querySelectorAll(".a-click");
 var seven = document.querySelector(".main");
 var eight = document.querySelector(".img-header");
 var nine = document.querySelector(".footer");
@@ -17,7 +17,7 @@ two.addEventListener("click", changeOne);
 
 var burgerMobile = function () {
   three.classList.toggle("is-open");
-  five.classList.toggle("is-close");
+  five.classList.toggle("is-covert");
   seven.classList.toggle("is-covert");
   eight.classList.toggle("is-covert");
   nine.classList.toggle("is-covert");
@@ -25,13 +25,6 @@ var burgerMobile = function () {
 
 four.addEventListener("click", burgerMobile);
 
-var burgerMobileClose = function () {
-  three.classList.toggle("is-covert");
-  seven.classList.toggle("is-visible");
-  eight.classList.toggle("is-visible");
-  nine.classList.toggle("is-visible");
-  five.classList.toggle("is-visible");
-  ten.classList.toggle("is-visible");
-};
-
-six.addEventListener("click", burgerMobileClose);
+six.forEach(function (link) {
+  link.addEventListener("click", burgerMobile);
+});
